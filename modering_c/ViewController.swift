51e688c6fd676328828e5fe660f875afd173d36d
Takumi Ooshiro <e165703@ie.u-ryukyu.ac.jp>
2017-11-16 11:22:03 +0900

    Initial Commit

---
+++ modering_c/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  modering_c
+//
+//  Created by 大城　拓千 on 2017/11/16.
+//  Copyright © 2017年 大城　拓千. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
